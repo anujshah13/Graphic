b5ebdd4dd8e381443f04c963944faad8dd1f5e1f
Anuj Shah <anujshah13@Anujs-MacBook-Pro.local>
2013-10-14 13:23:05 -0400

    Initial Commit

---
+++ Graphic/GraphicAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GraphicAppDelegate.h
+//  Graphic
+//
+//  Created by Anuj Shah on 10/14/13.
+//  Copyright (c) 2013 Anuj Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphicAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Graphic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Graphic
+//
+//  Created by Anuj Shah on 10/14/13.
+//  Copyright (c) 2013 Anuj Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GraphicAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GraphicAppDelegate class]));
+    }
+}
